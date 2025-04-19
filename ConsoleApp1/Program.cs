7db5ce9e3c45abb0538b74b942b5391ca46b0965
mstolovich <mstolovych@gmail.com>
2025-04-19 21:07:11 +0200

    casting types exercises

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+float total = 0f;
+string message = "";
+
+foreach (string value in values)
+{
+    if (float.TryParse(value, out float number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Total: {total}");
+Console.WriteLine($"Message: {message}");
