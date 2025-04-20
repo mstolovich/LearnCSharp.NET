a59dc7cd657688733c9e7e2643104e53186caa47
mstolovich <mstolovych@gmail.com>
2025-04-20 09:56:56 +0200

    narrowing and widening casting

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,18 +1,31 @@
-﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-float total = 0f;
-string message = "";
+﻿// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// float total = 0f;
+// string message = "";
 
-foreach (string value in values)
-{
-    if (float.TryParse(value, out float number))
-    {
-        total += number;
-    }
-    else
-    {
-        message += value;
-    }
-}
+// foreach (string value in values)
+// {
+//     if (float.TryParse(value, out float number))
+//     {
+//         total += number;
+//     }
+//     else
+//     {
+//         message += value;
+//     }
+// }
 
-Console.WriteLine($"Total: {total}");
-Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {total}");
+// Console.WriteLine($"Message: {message}");
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
