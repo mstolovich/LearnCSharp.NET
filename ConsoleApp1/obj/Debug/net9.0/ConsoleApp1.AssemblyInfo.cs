7c1061ca60e9b2d6296a9933e4f13964ac9912b0
mstolovich <mstolovych@gmail.com>
2025-05-15 15:51:24 +0200

    a mini-game starter

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc54043ef0d3689d445d8f29908b14a92033ecc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff0a366aed0b95861985d5490c96866d691688a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PettingZoo/obj/Debug/net9.0/PettingZoo.AssemblyInfo.cs
+++ PettingZoo/obj/Debug/net9.0/PettingZoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PettingZoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b343d711e5bc7aedbc27cdcd37a16cc1bb769c3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff0a366aed0b95861985d5490c96866d691688a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PettingZoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PettingZoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
